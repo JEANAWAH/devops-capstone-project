ccda6c0784c5f9853cd4d7d1b8317ea87126a930
JEANAWAH <awahnji100@gmail.com>
2025-03-07 14:27:30 -0500

    added link task

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -124,10 +124,9 @@ class TestAccountService(TestCase):
         self.assertEqual(response.status_code, status.HTTP_415_UNSUPPORTED_MEDIA_TYPE)
 
     def test_get_account_not_found(self):
-            """It should not Read an Account that is not found"""
-            resp = self.client.get(f"{BASE_URL}/0")
-            self.assertEqual(resp.status_code, status.HTTP_404_NOT_FOUND)
-
+        """It should not Read an Account that is not found"""
+        resp = self.client.get(f"{BASE_URL}/0")
+        self.assertEqual(resp.status_code, status.HTTP_404_NOT_FOUND)
 
     def test_get_account(self):
         """It should Read a single Account"""
@@ -138,4 +137,3 @@ class TestAccountService(TestCase):
         self.assertEqual(resp.status_code, status.HTTP_200_OK)
         data = resp.get_json()
         self.assertEqual(data["name"], account.name)
-#This is the last line
